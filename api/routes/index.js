8b402e004b2212cbf0816800425d955e16c64a9b
LingLing <xialingling0921@gmail.com>
2025-05-27 08:34:52 +0800

    fix: remove unuseless code

--- api/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/hello', function(req, res, next) {
-  res.json({ message: 'Hello Node.js' });
-});
-
-module.exports = router;
